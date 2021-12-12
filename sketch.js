3f07d9a42b42c4f47723c2889539294aad3a9559
Pavly O. Halim <49916453+pavlyhalim@users.noreply.github.com>
2021-12-12 17:45:08 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+
+let classifier;
+
+// Label
+let label = 'listening...';
+
+// Teachable Machine model URL:
+let soundModel = 'https://teachablemachine.withgoogle.com/models/5nsn2R5Yf/';
+
+
+function preload() {
+  // Load the model
+  classifier = ml5.soundClassifier(soundModel + 'model.json');
+}
+
+function setup() {
+  createCanvas(500, 500);
+  // Start classifying
+  // The sound model will continuously listen to the microphone
+  classifier.classify(gotResult);
+}
+
+function draw() {
+  background(0);
+  // Draw the label in the canvas
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height / 2);
+}
+
+
+// The model recognizing a sound will trigger this event
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+    label = results[0].label;
+
+    setTimeout(gotResult, 4000);
+	//setTimeout(gotResult, 1000);
+   
+
+}
